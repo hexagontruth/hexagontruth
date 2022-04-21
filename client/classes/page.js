2d101ceff6fc58ac656f9aee0f6e705ac2b509d8
graham <graham@hexnet.org>
2022-04-20 22:30:06 -0400

    Further fixes and expanded redirects

--- client/classes/page.js
+++ client/classes/page.js
@@ -1,9 +1,14 @@
 const PAGE_REDIRECTS = {
+  cryptovoxels: 'https://www.cryptovoxels.com/play?coords=W@375.5W,603S,0.5U',
   discord: 'https://discord.gg/t6hrz7S',
+  opensea: 'https://opensea.io/hexagontruth',
+  rarible: 'https://rarible.com/hexagontruth',
+  showtime: 'https://tryshowtime.com/hexagontruth',
+  superrare: 'https://superrare.co/hexagontruth',
   youtube: 'https://www.youtube.com/channel/UCf-ml0bmw7OJZHZCIB0cx3g',
-  sr: 'https://superrare.co/hexagontruth',
-  cryptoart: 'https://tryshowtime.com/hexagontruth',
   cv: 'https://www.cryptovoxels.com/play?coords=W@375.5W,603S,0.5U',
+  sr: 'https://superrare.co/hexagontruth',
+  yt: 'https://www.youtube.com/channel/UCf-ml0bmw7OJZHZCIB0cx3g',
 };
 
 const PROD_HOST = 'hexagontruth.com';
