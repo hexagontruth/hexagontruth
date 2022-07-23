3dd69f67cfe6a7240806d05b995149b0ceee7cb5
graham <graham@hexnet.org>
2022-07-23 13:30:41 -0400

    Add new cv redirect alias

--- client/classes/page.js
+++ client/classes/page.js
@@ -7,6 +7,7 @@ const PAGE_REDIRECTS = {
   superrare: 'https://superrare.co/hexagontruth',
   youtube: 'https://www.youtube.com/channel/UCf-ml0bmw7OJZHZCIB0cx3g',
   cv: 'https://www.cryptovoxels.com/play?coords=W@375.5W,603S,0.5U',
+  voxels: 'https://www.cryptovoxels.com/play?coords=W@375.5W,603S,0.5U',
   sr: 'https://superrare.co/hexagontruth',
   yt: 'https://www.youtube.com/channel/UCf-ml0bmw7OJZHZCIB0cx3g',
 };
